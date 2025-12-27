dabd3488b6456f9ce2ed20969f9d145be86c81a9
KukkiSaber1 <kukkisaber@gmail.com>
2025-12-28 01:48:41 +0800

    u22.3
    tried adding Universal Pipeline. It was simple to set than in quality.

--- project2/Assets/hmm/FireScript.cs
+++ project2/Assets/hmm/FireScript.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class FireScript : MonoBehaviour
 {
-    //public float burnDistance;
     public ParticleSystem smoke;
     public ParticleSystem fire;
     public ParticleSystem dissolve;
@@ -19,36 +18,50 @@ public class FireScript : MonoBehaviour
     public bool Burning { get; private set; } = false;
 
     private Vector3 oldPosition;
-
     private float smokeStart;
     private float fireStart;
 
-    // Start is called before the first frame update
+    // References to re-enable if you plan to reset/reuse this object
+    private MeshRenderer mr;
+    private SkinnedMeshRenderer smr;
+    private Collider[] colliders;
+
     void Start()
     {
-        //StartSmoke();
         oldPosition = transform.position;
+        mr = GetComponent<MeshRenderer>();
+        smr = GetComponentInChildren<SkinnedMeshRenderer>();
+        colliders = GetComponentsInChildren<Collider>();
 
         if (SmokeOnStart) StartSmoke();
     }
 
-    // Update is called once per frame
     void Update()
     {
-
         if (transform.position != oldPosition)
         {
             float d = Vector3.Magnitude(transform.position - oldPosition);
             FireManager.Instance.OnBurnableMoved(this.gameObject, d);
-
             oldPosition = transform.position;
         }
     }
 
+    // NEW: Extinguish fire when hit by water particles
+    // Note: The water Particle System must have 'Collision' module enabled
+    // Add this inside your FireScript class
+private void OnParticleCollision(GameObject other)
+{
+    // You can check if the particle belongs to a "Water" object
+    if (other.CompareTag("Water") || other.name.Contains("Extinguisher"))
+    {
+        StopBurning(); // Call the stop function we defined earlier
+    }
+}
+
+
     public void StartSmoke()
     {
         if (this.Burning) return;
-
         smokeStart = Time.time;
         this.StartCoroutine(Smoke(smokeStart));
     }
@@ -61,27 +74,26 @@ public class FireScript : MonoBehaviour
 
     public void StopBurning()
     {
-        if (this.Smoking && !SmokeOnStart)
+        StopAllCoroutines(); // Stop the countdown to dissolve
+
+        if (this.Smoking)
         {
             this.Smoking = false;
-            this.smoke.Stop();
+            if (smoke) smoke.Stop();
         }
 
         if (this.Burning)
         {
             this.Burning = false;
             FireManager.Instance.OnBurnStopped(this.gameObject);
-
-            this.fire.Stop();
+            if (fire) fire.Stop();
         }
     }
 
     IEnumerator Smoke(float t)
     {
         this.Smoking = true;
-
         this.smoke.Play();
-
         yield return new WaitForSeconds(SmokeDuration);
 
         if (this.Smoking && t == smokeStart)
@@ -93,10 +105,8 @@ public class FireScript : MonoBehaviour
 
     IEnumerator Fire(float t)
     {
-        //Debug.Log("Starting fire");
         this.Burning = true;
         this.fire.Play();
-
         FireManager.Instance.OnBurnStarted(this.gameObject);
 
         yield return new WaitForSeconds(0.5f);
@@ -117,46 +127,23 @@ public class FireScript : MonoBehaviour
 
     IEnumerator Dissolve()
     {
-        if (!Dissolvable) yield break;
-
-        // If it is no longer burning, then do not dissolve
-        // after previous bigfixes, should not be needed anymore
-        if (!this.Burning)
-        {
-            yield break;
-        }
+        if (!Dissolvable || !this.Burning) yield break;
 
         this.Burning = false;
         FireManager.Instance.OnBurnStopped(this.gameObject);
-
         this.fire.Stop();
+        
+        if(dissolve) dissolve.Play();
 
-        dissolve.Play();
+        // Disable visuals and physics
+        if (mr) mr.enabled = false;
+        if (smr) smr.enabled = false;
+        foreach (Collider c in colliders) c.enabled = false;
 
-        MeshRenderer mr = GetComponent<MeshRenderer>();
-        SkinnedMeshRenderer smr = GetComponentInChildren<SkinnedMeshRenderer>();
-
-        if (mr)
-        {
-            mr.enabled = false;
-        }
-
-        if (smr)
-        {
-            smr.enabled = false;
-        }
-
-        foreach (Collider c in GetComponents<Collider>())
-        {
-            c.enabled = false;
-        }
-
-        foreach (Collider c in GetComponentsInChildren<Collider>())
-        {
-            c.enabled = false;
-        }
-
+        // Wait for dissolve effect to finish
         yield return new WaitForSeconds(3f);
-        Destroy(gameObject);
+
+        // MODIFIED: Deactivate instead of Destroy to prevent mobile lag
+        gameObject.SetActive(false);
     }
 }
